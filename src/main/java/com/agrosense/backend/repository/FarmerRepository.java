f55513cb6f7b45f693357b5ebb3ab6c22799fc4a
DileepaPRA <prabhath0115@gmail.com>
2026-02-04 23:32:33 +0530

    Farmer Repository

---
+++ src/main/java/com/agrosense/backend/repository/FarmerRepository.java
@@ -0,0 +1,9 @@
+package com.agrosense.backend.repository;
+
+import com.agrosense.backend.entity.Farmer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FarmerRepository extends JpaRepository<Farmer, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByPhone(String phone);
+}
