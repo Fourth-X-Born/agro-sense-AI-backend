5024e20ced6f71fdb39d42f3be28b169dfea733d
DileepaPRA <prabhath0115@gmail.com>
2026-02-04 23:28:38 +0530

    service

---
+++ src/main/java/com/agrosense/backend/service/impl/AuthServiceImpl.java
@@ -0,0 +1,15 @@
+package com.agrosense.backend.service.impl;
+
+import com.agrosense.backend.dto.RegisterRequest;
+import com.agrosense.backend.dto.RegisterResponse;
+import com.agrosense.backend.service.AuthService;
+
+public class AuthServiceImpl implements AuthService{
+
+    @Override
+    public RegisterResponse register(RegisterRequest request) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'register'");
+    }
+    
+}
